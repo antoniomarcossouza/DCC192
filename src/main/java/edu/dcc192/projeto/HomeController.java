da4cfe2ac3ecd8683282f304421b13702790a562
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2024-12-04 18:10:43 -0300

    aula 04

--- src/main/java/edu/dcc192/projeto/HomeController.java
+++ src/main/java/edu/dcc192/projeto/HomeController.java
@@ -1,5 +1,6 @@
 package edu.dcc192.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,27 +8,43 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private GeradorSenha senha;
+
     @GetMapping("/")
-    public ModelAndView home() {
+    public ModelAndView captcha_home() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("captcha");
+        mv.addObject("senha", senha.GerarSenha());
+        return mv;
+    }
+
+    @GetMapping("login")
+    public ModelAndView login() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("login");
         return mv;
     }
 
+    @Autowired
+    private Dados dados;
+
     @GetMapping("menu")
-    public ModelAndView getMethodName(@RequestParam String name) {
+    public ModelAndView menu(@RequestParam String name) {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("index");
+        mv.setViewName("menu");
         mv.addObject("userName", name);
+        mv.addObject("dados", dados.pegaDados());
         return mv;
     }
 
     @GetMapping("sair")
-    public ModelAndView getMethodLogout() {
+    public ModelAndView captcha_logout() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("login");
+        mv.setViewName("captcha");
         mv.addObject("logout", true);
+        mv.addObject("senha", senha.GerarSenha());
         return mv;
     }
-
 }
