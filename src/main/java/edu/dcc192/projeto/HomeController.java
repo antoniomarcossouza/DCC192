8244690d1ad5a2cc0a71fa935183b720735d7714
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-01-28 21:14:22 -0300

    .

--- src/main/java/edu/dcc192/projeto/HomeController.java
+++ src/main/java/edu/dcc192/projeto/HomeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
